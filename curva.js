6bb1e7f1e187c804561981a3f6764f2132d3024c
Kymberlly <kymberllyglaucia@gmail.com>
2019-11-17 18:27:09 -0300

    Modificações no algoritmo e no estilo do programa.

--- curva.js
+++ curva.js
@@ -1,32 +1,38 @@
 var clicks     = [];
+var pFinal     = {};
 var contPontos = 0;
 
-//Inicializando tela onde serão plotados os
-//desenhos
-var canvas     = document.querySelector('canvas');
-var context    = canvas.getContext('2d');
+//Inicializando tela onde serão plotados os desenhos
+var canvas  = document.querySelector('canvas');
+var context = canvas.getContext('2d');
 
+/**
+ * Pega os clicks que o usuário fez na tela
+ * @param {event} event - Evento de click da tela
+ */
 function getClicks(event){
-    clicks.push({x: event.clientX, y: event.clientY});
+    //Permite criar, no maximo, 4 pontos para a curva de Bezier
+    if(contPontos <= 3){
+        clicks.push({x: event.clientX, y: event.clientY});
 
-    $('#pt'+contPontos+'x').val(event.clientX);
-    $('#pt'+contPontos+'y').val(event.clientY);
+        $('#pt'+contPontos+'x').val(event.clientX);
+        $('#pt'+contPontos+'y').val(event.clientY);
 
-    contPontos += 1;
+        contPontos += 1;
 
-    if(clicks.length > 0)
-        plotaPontos();
-     
-    if(clicks.length == 4)
-        desenhaCurva();
+        //Ao registrar o primeiro click, plota na tela para visualização do usuário
+        if(clicks.length <= 4)
+            plotaPontos();
+        
+        //Se houverem 4 pontos, plota a curva de Bezier
+        if(clicks.length == 4)
+            desenhaCurva();
+    }
 }
 
-//Permite no maximo criar 4 pontos para a reta
-if(contPontos <= 3)
-    document.addEventListener("click", getClicks);
-
-var pFinal = {}
-
+//Esperando evento de click do mouse para adicionar 
+//pontos e curva na tela
+canvas.addEventListener("click", getClicks);
 
 //Plota na tela os pontos clicados pelo usuário
 function plotaPontos(){
@@ -55,25 +61,33 @@ function mudaPonto(obj, posicao, eixo){
     desenhaCurva();
 }
 
+//Limpa a tela de desenho e os campos de coordenadas
+function limpaTela(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    contPontos = 0;
+    clicks = [];
+    $('.painel_controle input').val("");
+
+}
+
 //Desenha a curva de Bezier na tela seguindo
 //os pontos escolhidos anteriormente
 function desenhaCurva(){
 
     context.beginPath();
+
+    //Move para o primeiro ponto a ser plotado na tela
     context.moveTo(clicks[0].x, clicks[0].y);
-    for(var t=0; t <= 1; t += 0.01){
 
+    for(var u=0; u <= 1; u += 0.01){
         //Chama curva de Bezier para o parametro t e os pontos passados
-        curvaBezier(clicks[0], clicks[1], clicks[2], clicks[3], t, pFinal);
+        curvaBezier(clicks[0], clicks[1], clicks[2], clicks[3], u, pFinal);
+
+        //Desenha a linha no ponto calculado
         context.lineTo(pFinal.x, pFinal.y);
-
-        //Plota para cada ponto uma circunferencia na tela
-        //context.arc(pFinal.x, pFinal.y, 10, 1, Math.PI * 2, false);
-
-        //Desenha a circunferencia
-        
     }
 
+    //Desenha a curva de Bezier
     context.stroke();
 }
 
@@ -81,19 +95,23 @@ function desenhaCurva(){
 /**
  * 
  * @param {Object} p0 - Primeiro ponto escolhido pelo usuário
- * @param {Object} p1 - Primeiro ponto escolhido pelo usuário
- * @param {Object} p2 - Primeiro ponto escolhido pelo usuário 
- * @param {Object} p3 - Primeiro ponto escolhido pelo usuário 
+ * @param {Object} p1 - Segundo ponto escolhido pelo usuário
+ * @param {Object} p2 - Terceiro ponto escolhido pelo usuário 
+ * @param {Object} p3 - Quarto ponto escolhido pelo usuário 
  * @param {int} t - Parâmetro da curva
  * @param {Object} pFinal - Ponto final da curva a ser plotado
  */
-function curvaBezier(p0, p1, p2, p3, t, pFinal){
-    pFinal = pFinal || {};
-    pFinal.x = Math.pow(1 - t, 3) * p0.x + 
-               Math.pow(1 - t, 2) * 3 * t * p1.x + (1 - t) * 3 * t * t * p2.x + t * t * t * p3.x;
+function curvaBezier(p0, p1, p2, p3, u, pFinal){
 
-    pFinal.y = Math.pow(1 - t, 3) * p0.y + 
-               Math.pow(1 - t, 2) * 3 * t * p1.y + (1 - t) * 3 * t * t * p2.y + t * t * t * p3.y;
+    //Se o ponto final ja existir, utiliza ele para calcular
+    //Se não existir, seta para um objeto vazio
+    pFinal = pFinal || {};
+
+    pFinal.x = Math.pow(1 - u, 3) * p0.x + 
+               Math.pow(1 - u, 2) * 3 * u * p1.x + (1 - u) * 3 * u * u * p2.x + u * u * u * p3.x;
+
+    pFinal.y = Math.pow(1 - u, 3) * p0.y + 
+               Math.pow(1 - u, 2) * 3 * u * p1.y + (1 - u) * 3 * u * u * p2.y + u * u * u * p3.y;
 
     return pFinal;
 }
